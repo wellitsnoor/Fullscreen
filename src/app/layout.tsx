1330c19f4c08f8c2b3c46bbd0366f756d44f919f
Kanwarnoor Singh <famousfortech500@gmail.com>
2024-10-16 20:41:59 +0530

    icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <link rel="shortcut icon" href="/logo.png" type="image/x-icon" />
       <body>
         {children}
       </body>
